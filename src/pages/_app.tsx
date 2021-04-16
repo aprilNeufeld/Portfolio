9249716f3650b7071a4c30ce0c36fd5fdb03b8f2
April <april.neufeld@gmail.com>
2021-04-16 16:27:55 -0600

    Add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,25 @@ import { theme } from '../styles';
 import { ThemeProvider } from '@material-ui/core';
 import { ConnectedRouter } from 'connected-next-router';
 import { useStore } from '../store/configureStore';
+import { useRouter } from 'next/router';
 import { Provider } from 'react-redux';
+import * as gtag from '../lib/gtag';
 
 const PortfolioApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+
 	const store = useStore(pageProps.initialReduxState);
+	const router = useRouter();
+
+	React.useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			gtag.pageview(url)
+		}
+		router.events.on('routeChangeComplete', handleRouteChange)
+
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange)
+		}
+	}, [router.events])
 
 	return (
 		<Provider store={store}>
