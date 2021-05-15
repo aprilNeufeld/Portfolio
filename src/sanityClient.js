e29500e7b09bfbd819171f1926566d978a7fb87e
tricksterCodess <tricksterCodess@users.noreply.github.com>
2021-05-15 14:19:59 -0500

    Begin integrating sanity preview

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,9 +1,16 @@
 ﻿import sanityClient from "@sanity/client";
 
 export default sanityClient({
-	projectId: "vachv01z", 
+	projectId: process.env.SANITY_PROJECT_ID, 
 	dataset: "production", 
 	apiVersion: '2021-03-31',
-	token: process.env.SANITY_POST_PREVIEW_TOKEN,
 	useCdn: true,
 });
+
+export const sanityPreviewClient = ({
+	projectId: "vachv01z",
+	dataset: "production",
+	apiVersion: '2021-03-31',
+	token: process.env.SANITY_POST_PREVIEW_TOKEN,
+	useCdn: false,
+})
