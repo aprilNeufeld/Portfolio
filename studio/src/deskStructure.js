9bf03eda7c6e1a2599f8774376726b0697947e84
April <april.neufeld@gmail.com>
2021-05-19 10:53:20 -0500

    Initialize split pane previews

---
+++ studio/src/deskStructure.js
@@ -0,0 +1,27 @@
+﻿import S from "@sanity/desk-tool/structure-builder";
+import PostPreview from "../components/previews/PostPreview";
+
+export default () =>
+	S.list()
+		.title("Blog")
+		.items([
+			...S.documentTypeListItems(),
+		]);
+
+export const getDefaultDocumentNode = (props) => {
+	/**
+	 * Here you can define fallback views for document types without
+	 * a structure definition for the document node. If you want different
+	 * fallbacks for different types, or document values (e.g. if there is a slug present)
+	 * you can set up that logic in here too.
+	 * https://www.sanity.io/docs/structure-builder-reference#getdefaultdocumentnode-97e44ce262c9
+	 */
+	const { schemaType } = props;
+	if (schemaType === "post") {
+		return S.document().views([
+			S.view.form(),
+			S.view.component(PostPreview).title("Preview"),
+		]);
+	}
+	return S.document().views([S.view.form()]);
+};
---
+++ studio/src/previews/PostPreview.js
@@ -0,0 +1,27 @@
+﻿import React from "react";
+import styles from "./IframePreview.css";
+import { secret } from './secret';
+
+export default function PostPreview(props) {
+	const { displayed } = props.document;
+	const { slug } = displayed?.slug?.current;
+
+	if (!slug) {
+		return <div>The product needs a slug before it can be previewed.</div>;
+	}
+
+	const url = process.env.NODE_ENV === "production"
+		? `../..`
+		: `http://localhost:3000`;
+
+	return (
+		<div className={styles.componentWrapper}>
+			<div className={styles.iframeContainer}>
+				<iframe
+					src={`${url}/api/preview?secret=${secret}&slug=${slug}`}
+					frameBorder={"0"}
+				/>
+			</div>
+		</div>
+	);
+}
