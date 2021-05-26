b2ea2e37392dd1b0a493f1ee47739fcbace8634a
April <april.neufeld@gmail.com>
2021-05-26 10:09:51 -0500

    Attempt alternate rewrites

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
-﻿const STUDIO_REWRITE = {
-  source: '/studio/:path*',
-  destination:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3333/studio/:path*'
-          : 'https://studio-beryl.vercel.app/index.html',
+﻿/*
+const STUDIO_REWRITE = {
+	source: '/studio/:path*',
+	destination:
+		process.env.NODE_ENV === 'development'
+			? 'http://localhost:3333/:path*'
+			: 'https://studio-beryl.vercel.app/index.html',
+	basePath: 'false',
 }
 
 module.exports = {
-  rewrites: () => [STUDIO_REWRITE],
+	rewrites: () => [STUDIO_REWRITE],
+}
+*/
+
+module.exports = {
+	async rewrites() {
+		return {
+			fallback: [
+				{
+					source: '/studio/:path*',
+					destination: `https://studio-beryl.vercel.app/:path*`,
+				},
+			],
+		}
+	},
 }
