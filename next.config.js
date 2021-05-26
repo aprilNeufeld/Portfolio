8f5f2f92972515939cc7362870d9c33141704d5b
April <april.neufeld@gmail.com>
2021-05-26 10:18:25 -0500

    Remove wildcard path from rewrites

--- next.config.js
+++ next.config.js
@@ -18,8 +18,8 @@ module.exports = {
 		return {
 			fallback: [
 				{
-					source: '/studio/:path*',
-					destination: `https://studio-beryl.vercel.app/:path*`,
+					source: '/studio',
+					destination: 'https://studio-beryl.vercel.app',
 				},
 			],
 		}
