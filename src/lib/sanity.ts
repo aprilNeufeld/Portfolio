1ebb4ccdb6fd24066ce263e9c7048cd9f4c57309
tricksterCodess <tricksterCodess@users.noreply.github.com>
2022-07-04 16:21:20 -0500

    Update Next version and fix hooks errors

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,49 +1,51 @@
-import { SanityClient } from '@sanity/client';
-import { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { SanityClient } from "@sanity/client";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import {
-	createClient,
-	createImageUrlBuilder,
-	createPreviewSubscriptionHook,
-    ProjectConfig
-} from 'next-sanity';
+  createClient,
+  createImageUrlBuilder,
+  createPreviewSubscriptionHook,
+  ProjectConfig,
+} from "next-sanity";
 
-const API_VERSION = '2021-03-25';
+const API_VERSION = "2021-03-25";
 
 if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
-	throw Error("The Project ID is not set. Check your environment variables.");
+  throw Error("The Project ID is not set. Check your environment variables.");
 }
 if (!process.env.NEXT_PUBLIC_SANITY_DATASET) {
-	throw Error("The Dataset name is not set. Check your environment variables.");
+  throw Error("The Dataset name is not set. Check your environment variables.");
 }
 
 const config: ProjectConfig = {
-	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-}
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+};
 
 /**
  * Set up a helper function for generating Image URLs with only the asset reference data in your documents.
  * Read more: https://www.sanity.io/docs/image-url
  **/
-export const urlFor = (source: SanityImageSource) => createImageUrlBuilder(config).image(source)
+export const urlFor = (source: SanityImageSource) =>
+  createImageUrlBuilder(config).image(source);
 
 // Set up the live preview subscription hook
-export const usePreviewSubscription = createPreviewSubscriptionHook(config)
+export const usePreviewSubscription = createPreviewSubscriptionHook(config);
 
 // Create the normal client
 export const sanityClient: SanityClient = createClient({
-	...config,
-	apiVersion: API_VERSION,
-	useCdn: true,
+  ...config,
+  apiVersion: API_VERSION,
+  useCdn: true,
 });
 
 // Create the preview client
 export const previewClient: SanityClient = createClient({
-	...config,
-	apiVersion: API_VERSION,
-	useCdn: false,
-	token: process.env.SANITY_API_TOKEN,
-	//withCredentials: true,
-})
+  ...config,
+  apiVersion: API_VERSION,
+  useCdn: false,
+  token: process.env.SANITY_API_TOKEN,
+  //withCredentials: true,
+});
 
-export const getClient = (usePreview: boolean) => (usePreview ? previewClient : sanityClient);
+export const getClient = (usePreview: boolean) =>
+  usePreview ? previewClient : sanityClient;
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,77 +1,77 @@
-import * as React from 'react';
-import ErrorPage from 'next/error';
-import { GetStaticProps, GetStaticPaths } from 'next';
-import { useRouter } from 'next/router'
-import Link from 'next/link';
-import Layout from '../../components/Layout';
-import { getClient } from '../../lib/sanity';
-import BlockRenderer from '../../components/BlockRenderer';
+import * as React from "react";
+import ErrorPage from "next/error";
+import { GetStaticProps, GetStaticPaths } from "next";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Layout from "../../components/Layout";
+import { getClient } from "../../lib/sanity";
+import BlockRenderer from "../../components/BlockRenderer";
 import {
-	FacebookShareButton,
-	TwitterShareButton,
-	RedditShareButton,
-	LinkedinShareButton,
-} from 'react-share';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import RedditIcon from '@material-ui/icons/Reddit';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+  FacebookShareButton,
+  TwitterShareButton,
+  RedditShareButton,
+  LinkedinShareButton,
+} from "react-share";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import RedditIcon from "@material-ui/icons/Reddit";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import {
-	Box,
-	Breadcrumbs,
-	CardMedia,
-	Divider,
-	Typography,
-	makeStyles,
-	Theme,
-	createStyles,
-	useTheme
-} from '@material-ui/core';
-import PageTitle from '../../components/PageTitle';
-import { fetchUserState } from '../../lib/staticFetching';
-import { usePreviewSubscription, urlFor } from '../../lib/sanity';
-import { SanityClient } from '@sanity/client';
-import { groq } from 'next-sanity';
+  Box,
+  Breadcrumbs,
+  CardMedia,
+  Divider,
+  Typography,
+  makeStyles,
+  Theme,
+  createStyles,
+  useTheme,
+} from "@material-ui/core";
+import PageTitle from "../../components/PageTitle";
+import { fetchUserState } from "../../lib/staticFetching";
+import { usePreviewSubscription, urlFor } from "../../lib/sanity";
+import { SanityClient } from "@sanity/client";
+import { groq } from "next-sanity";
 
 const useStyles = makeStyles((theme: Theme) => {
-	return createStyles({
-		blockQuote: {
-			marginBottom: '1rem',
-		},
-		breadcrumbs: {
-			marginBottom: theme.spacing(4),
-			'& a': {
-				textDecoration: 'none',
-			},
-		},
-		media: {
-			height: 0,
-			//paddingTop: '56.25%', // 16:9
-			paddingTop: '62%',
-			marginBottom: theme.spacing(4),
-		},
-		postBodyText: {
-			position: 'relative',
-			lineHeight: 1.4,
-			'& p': {
-				marginBottom: theme.spacing(5)
-			},
-		},
-		postDetails: {
-			color: theme.palette.text.secondary,
-		},
-		shareButtonsContainer: {
-			display: 'flex',
-			flexDirection: 'row',
-			paddingTop: theme.spacing(2),
-		},
-		shareButton: {
-			marginLeft: theme.spacing(1),
-		},
-		shareIcon: {
-			color: theme.palette.action.disabled
-		},
-	});
+  return createStyles({
+    blockQuote: {
+      marginBottom: "1rem",
+    },
+    breadcrumbs: {
+      marginBottom: theme.spacing(4),
+      "& a": {
+        textDecoration: "none",
+      },
+    },
+    media: {
+      height: 0,
+      //paddingTop: '56.25%', // 16:9
+      paddingTop: "62%",
+      marginBottom: theme.spacing(4),
+    },
+    postBodyText: {
+      position: "relative",
+      lineHeight: 1.4,
+      "& p": {
+        marginBottom: theme.spacing(5),
+      },
+    },
+    postDetails: {
+      color: theme.palette.text.secondary,
+    },
+    shareButtonsContainer: {
+      display: "flex",
+      flexDirection: "row",
+      paddingTop: theme.spacing(2),
+    },
+    shareButton: {
+      marginLeft: theme.spacing(1),
+    },
+    shareIcon: {
+      color: theme.palette.action.disabled,
+    },
+  });
 });
 
 export const postQuery = groq`*[_type == "post" && slug.current == $slug] | order(_updatedAt desc)[0]{
@@ -86,27 +86,27 @@ export const postQuery = groq`*[_type == "post" && slug.current == $slug] | orde
 
 // The structure of a single post's data
 type PostType = {
-	title: string,
-	slug: string,
-	author: string,
-	mainImage: any,
-	publishedAt: string,
-	body: any
-}
+  title: string;
+  slug: string;
+  author: string;
+  mainImage: any;
+  publishedAt: string;
+  body: any;
+};
 
 const defaults: PostType = {
-	title: 'Title',
-	slug: 'slug',
-	author: 'Author',
-	mainImage: '/images/placeholder.png',
-	publishedAt: '2000-01-01',
-	body: []
-}
+  title: "Title",
+  slug: "slug",
+  author: "Author",
+  mainImage: "/images/placeholder.png",
+  publishedAt: "2000-01-01",
+  body: [],
+};
 
 interface Props {
-	pageData: { post: PostType };
-	shareUrl: string;
-	preview: boolean;
+  pageData: { post: PostType };
+  shareUrl: string;
+  preview: boolean;
 }
 /*
  * LINKS:
@@ -118,169 +118,165 @@ interface Props {
  * https://www.sanity.io/docs/http-mutations
  * https://stackoverflow.com/questions/63221030/access-preview-api-from-a-cms-in-next-js
  * https://github.com/sanity-io/next-sanity/issues/16
- * 
+ *
  */
 const Post: React.FC<Props> = ({ pageData, shareUrl, preview }) => {
+  // Get our styles
+  const classes = useStyles(useTheme());
 
-	// This is used for live preview with Sanity - the first time we load a 
-	// nonexistent post, router.isFallback will be true, and we show the page with
-	// placeholder data until we can get data from the usePreviewSubscription hook. 
-	// After usePreviewSubscription rerenders the page, if there is still no page data
-	// (i.e. there is no preview post to render), we default to a 404.
-	const router = useRouter();
-	if ((!router.isFallback || !preview) && (!pageData?.post?.slug)) {
-		return <ErrorPage statusCode={404} />
-	}
+  // Load the live preview data
+  const { data: post = defaults } = usePreviewSubscription(postQuery, {
+    params: { slug: pageData?.post?.slug },
+    initialData: pageData?.post, // we preserve the initial page data if it's a prerendered page
+    enabled: preview,
+  });
 
-	// Get our styles
-	const classes = useStyles(useTheme());
+  // This is used for live preview with Sanity - the first time we load a
+  // nonexistent post, router.isFallback will be true, and we show the page with
+  // placeholder data until we can get data from the usePreviewSubscription hook.
+  // After usePreviewSubscription rerenders the page, if there is still no page data
+  // (i.e. there is no preview post to render), we default to a 404.
+  const router = useRouter();
 
-	// Load the live preview data 
-	const { data: post = defaults } = usePreviewSubscription(postQuery, {
-		params: { slug: pageData.post.slug },
-		initialData: pageData.post, // we preserve the initial page data if it's a prerendered page
-		enabled: preview,
-	})
+  if ((!router.isFallback || !preview) && !pageData?.post?.slug) {
+    return <ErrorPage statusCode={404} />;
+  }
 
-	// Set defaults to avoid errors
-	const {
-		author = defaults.author,
-		body = defaults.body,
-		mainImage = defaults.mainImage,
-		publishedAt = defaults.publishedAt,
-		title = defaults.title,
-	} = post;
+  // Set defaults to avoid errors
+  const {
+    author = defaults.author,
+    body = defaults.body,
+    mainImage = defaults.mainImage,
+    publishedAt = defaults.publishedAt,
+    title = defaults.title,
+  } = post;
 
-	// Function that formats our post's date
-	const formatDate = (datetime: string): string => {
-		const date: Date = new Date(datetime);
-		return date.toLocaleDateString('en-US', {
-			year: 'numeric', month: 'long', day: 'numeric'
-		});
-	}
+  // Function that formats our post's date
+  const formatDate = (datetime: string): string => {
+    const date: Date = new Date(datetime);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
 
-	return (
-		<React.Fragment>
-			<Layout pageTitle={title} preview={preview}>
-				<React.Fragment>
-					<Breadcrumbs aria-label="breadcrumb" className={classes.breadcrumbs}>
-						<Link href="/">
-							Home
-							</Link>
-						<Link href="/blog">
-							Blog
-							</Link>
-						<Typography color="textPrimary"></Typography>
-					</Breadcrumbs>
-					<PageTitle text={title} />
-					<CardMedia
-						image={urlFor(mainImage).url() ?? mainImage}
-						className={classes.media}
-					/>
-					<Typography
-						variant='body1'
-						className={classes.postDetails}
-					>
-						{'by ' + author + ' on ' + formatDate(publishedAt)}
-					</Typography>
-					<Typography
-						variant='body1'
-						className={classes.postBodyText}
-						component='div'
-					>
-						<BlockRenderer
-							content={body}
-						/>
-					</Typography>
-					<Divider />
-					<Box className={classes.shareButtonsContainer}>
-						<FacebookShareButton
-							url={shareUrl}
-							quote={title}
-							className={classes.shareButton}
-						>
-							<FacebookIcon className={classes.shareIcon} />
-						</FacebookShareButton>
-						<TwitterShareButton
-							url={shareUrl}
-							title={title}
-							className={classes.shareButton}
-						>
-							<TwitterIcon className={classes.shareIcon} />
-						</TwitterShareButton>
-						<RedditShareButton
-							url={shareUrl}
-							title={title}
-							windowWidth={660}
-							windowHeight={460}
-							className={classes.shareButton}
-						>
-							<RedditIcon className={classes.shareIcon} />
-						</RedditShareButton>
-						<LinkedinShareButton
-							url={shareUrl}
-							title={title}
-							className={classes.shareButton}
-						>
-							<LinkedInIcon className={classes.shareIcon} />
-						</LinkedinShareButton>
-					</Box>
-				</React.Fragment>
-			</Layout >
-		</React.Fragment >
-	)
+  return (
+    <React.Fragment>
+      <Layout pageTitle={title} preview={preview}>
+        <React.Fragment>
+          <Breadcrumbs aria-label="breadcrumb" className={classes.breadcrumbs}>
+            <Link href="/">Home</Link>
+            <Link href="/blog">Blog</Link>
+            <Typography color="textPrimary"></Typography>
+          </Breadcrumbs>
+          <PageTitle text={title} />
+          <CardMedia
+            image={urlFor(mainImage).url() ?? mainImage}
+            className={classes.media}
+          />
+          <Typography variant="body1" className={classes.postDetails}>
+            {"by " + author + " on " + formatDate(publishedAt)}
+          </Typography>
+          <Typography
+            variant="body1"
+            className={classes.postBodyText}
+            component="div"
+          >
+            <BlockRenderer content={body} />
+          </Typography>
+          <Divider />
+          <Box className={classes.shareButtonsContainer}>
+            <FacebookShareButton
+              url={shareUrl}
+              quote={title}
+              className={classes.shareButton}
+            >
+              <FacebookIcon className={classes.shareIcon} />
+            </FacebookShareButton>
+            <TwitterShareButton
+              url={shareUrl}
+              title={title}
+              className={classes.shareButton}
+            >
+              <TwitterIcon className={classes.shareIcon} />
+            </TwitterShareButton>
+            <RedditShareButton
+              url={shareUrl}
+              title={title}
+              windowWidth={660}
+              windowHeight={460}
+              className={classes.shareButton}
+            >
+              <RedditIcon className={classes.shareIcon} />
+            </RedditShareButton>
+            <LinkedinShareButton
+              url={shareUrl}
+              title={title}
+              className={classes.shareButton}
+            >
+              <LinkedInIcon className={classes.shareIcon} />
+            </LinkedinShareButton>
+          </Box>
+        </React.Fragment>
+      </Layout>
+    </React.Fragment>
+  );
 };
 
 /*
  * Using the paths returned by getStaticPaths, here we get the information
  * for a single post that will be displayed on this page.
  */
-export const getStaticProps: GetStaticProps = async ({ params = {}, preview = false }) => {
-	const sanityClient: SanityClient = getClient(preview);
+export const getStaticProps: GetStaticProps = async ({
+  params = {},
+  preview = false,
+}) => {
+  const sanityClient: SanityClient = getClient(preview);
 
-	const { slug } = params;
-	const post = await sanityClient.fetch(postQuery, { slug });
-	const shareUrl = `https://www.tricksterCodess.com/post/${slug}`;
-	let initialReduxState = undefined;
+  const { slug } = params;
+  const post = await sanityClient.fetch(postQuery, { slug });
+  const shareUrl = `https://www.tricksterCodess.com/post/${slug}`;
+  let initialReduxState = undefined;
 
-	// Only bother initializing the user state if it isn't a post
-	// preview page, because we've eliminated the header that uses the user
-	// information on the post preview to speed things up
-	if (!preview) {
-		initialReduxState = {
-			user: await fetchUserState()
-		}
-	}
-	
-	return {
-		props: {
-			pageData: {
-				post,
-			},
-			shareUrl,
-			preview,
-			initialReduxState,
-		},
-		revalidate: 1
-	};
-}
+  // Only bother initializing the user state if it isn't a post
+  // preview page, because we've eliminated the header that uses the user
+  // information on the post preview to speed things up
+  if (!preview) {
+    initialReduxState = {
+      user: await fetchUserState(),
+    };
+  }
+
+  return {
+    props: {
+      pageData: {
+        post,
+      },
+      shareUrl,
+      preview,
+      initialReduxState,
+    },
+    revalidate: 1,
+  };
+};
 
 /*
  * Called at build time, this function gets the paths of all our
  * individual blog posts.
  */
 export const getStaticPaths: GetStaticPaths = async () => {
-	const sanityClient: SanityClient = getClient(false);
+  const sanityClient: SanityClient = getClient(false);
 
-	// Call an external API endpoint to get post slugs
-	const slugs = await sanityClient.fetch(
-		groq`*[_type == "post" && defined(slug.current)][].slug.current`
-	);
+  // Call an external API endpoint to get post slugs
+  const slugs = await sanityClient.fetch(
+    groq`*[_type == "post" && defined(slug.current)][].slug.current`
+  );
 
-	// Get the paths we want to pre-render based on post slugs
-	const paths = slugs.map((slug: any) => ({ params: { slug } }));
+  // Get the paths we want to pre-render based on post slugs
+  const paths = slugs.map((slug: any) => ({ params: { slug } }));
 
-	return { paths, fallback: true, }
-}
+  return { paths, fallback: true };
+};
 
 export default Post;
